b5cc869bc6ac4763f57a99f8994a887164acb3fe
souheibriache <gs_riache@esi.dz>
2024-09-22 12:53:04 +0200

    project structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,20 @@
 import "./App.css";
 
 function App() {
-  return <></>;
+  return (
+    <div className="w-full h-full font-mono">
+      <div className="w-full border-0 justify-center">
+        <NavBar />
+        <div className="w-full bg-[#10172A] pt-10">
+          <Home />
+          <AboutMe />
+          <Experience />
+          <Projects />
+          <Contact />
+        </div>
+        <Footer />
+      </div>
+    </div>
+  );
 }
-
 export default App;
